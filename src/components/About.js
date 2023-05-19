47ef9d63d3d17229896609ede7f7246f6788bc4c
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-05-18 22:06:02 -0600

    added more projects

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ export default function About() {
         <div>   
           <h3>Scarlet Bowen, Ph.D.</h3>
           <h4>Full Stack Web Developer</h4>
-          <p className="aboutMe"> After pursuing careers as a literature faculty member and DEI practitioner, I became interested in bringing an analytical mindset to programming and data analytics. Currently, I am obtaining a certificate in Full Stack Web Development from UC-Irvine. I enjoy bringing ideas to fruition through problem-solving, clear communication, and inclusive design.</p>
+          <p className="aboutMe"> After pursuing careers as a literature faculty member and DEI practitioner, I became interested in applying my analytical training to programming and data analytics. Currently, I am obtaining a certificate in Full Stack Web Development from UC-Irvine. I enjoy bringing development ideas to fruition through problem-solving, clear communication, and inclusive design.</p>
         </div>
       </section>
     </div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,17 +4,20 @@ import { Container, Row, Col } from 'react-bootstrap';
 import techblog from '../photos/techblog.png';
 import moviemood from '../photos/moviemood.png';
 import socialmedia from '../photos/socialmedia.png';
+import projectpilot from '../photos/projectpilot.png';
+import weather from '../photos/weather.png';
+import codingquiz from '../photos/codingquiz.png';
 
 const Portfolio = () => (
   <div className="projects text-center">
-    <h1>My recent work</h1>
+    <h1>My Recent Work</h1>
     <Container>
       <Row>
         <Col md='4'>
           <Project
             name={'Tech Blog'}
             technology={'This application follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication. '}
-            description={'a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well'}
+            description={'This is a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well.'}
             link={'https://desolate-depths-78640.herokuapp.com/'}
             repo={'https://github.com/ScarletBowen/Tech-Blog'}
             image={techblog}
@@ -23,8 +26,8 @@ const Portfolio = () => (
         <Col md='4'>
           <Project
             name={'Movie Mood--a collaborative project'}
-            technology={'Uses third-party API to access movie data.  This application uses HTML, CSS, and Javascript.'}
-            description={'a web application that allows a user to find 3 movie recommendations after they have selected what mood they are feeling.'}
+            technology={'This application uses third-party APIs to access movie data.  This application uses HTML, CSS, and Javascript.'}
+            description={'A web application that allows a user to find 3 movie recommendations after they have selected what mood they are feeling.'}
             link={'https://corradoj02.github.io/Movie-Mood/'}
             repo={'https://github.com/corradoj02/Movie-Mood'}
             image={moviemood}
@@ -34,13 +37,46 @@ const Portfolio = () => (
           <Project
             name={'Social Media API'}
             technology={'This is a back-end application that uses MongoDB and the Mongoose ODM to interact with a NoSQL database.'}
-            description={'an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.'}
+            description={'This is an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.'}
             link={'https://drive.google.com/file/d/1G8SAQWXwZSsnTBPRepuKRwBt9g5lYBEz/view'}
             repo={'https://github.com/ScarletBowen/social-media-MongoDB'}
             image={socialmedia}
           />
         </Col>
-      </Row>
+      
+      <Col md='4'>
+          <Project
+            name={'Project Pilot--a collaborative project'}
+            technology={'This is a full-stack application that uses Bootstrap, Node.js, Express.js, MySQL, Sequelize, and Handlebars.js.'}
+            description={'Designed for freelancers, this application allows users to track their projects, clients, and hours worked.'}
+            link={'coming soon'}
+            repo={'https://github.com/tcmiller30/project-pilot'}
+            image={projectpilot}
+          />
+        </Col>
+     
+      <Col md='4'>
+          <Project
+            name={'Weather Planner'}
+            technology={'This application uses a third-party API to access weather data, using HTML, CSS, and Javascript.'}
+            description={'A weather dashboard that runs in the browser and features dynamically updated HTML and CSS.'}
+            link={'https://scarletbowen.github.io/Weather-Planner/'}
+            repo={'https://github.com/ScarletBowen/Weather-Planner'}
+            image={weather}
+          />
+        </Col>
+   
+      <Col md='4'>
+      <Project
+        name={'Coding Quiz'}
+        technology={'This application uses HTML, CSS, and Javascript.'}
+        description={'A timed coding quiz with multiple-choice questions. The application stores user scores in local storage and retrieves the cumulative high score when the game is over.'}
+        link={' https://scarletbowen.github.io/Code-Quiz/'}
+        repo={'https://github.com/ScarletBowen/Code-Quiz'}
+        image={codingquiz}
+      />
+    </Col>
+  </Row>
     </Container>
   </div>
 );
